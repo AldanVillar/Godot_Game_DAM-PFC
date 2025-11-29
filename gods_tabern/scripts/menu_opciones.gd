5c056da5e9f9c25f6d75c1643127bac72de75552
Aldan <avillars23@fpcoruna.afundacion.com>
2025-11-29 18:43:02 +0100

    music

--- gods_tabern/scripts/menu_opciones.gd
+++ gods_tabern/scripts/menu_opciones.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+func _ready() -> void:
+	$MainMusic.play()
 
 func _on_h_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),linear_to_db(value))
--- gods_tabern/scripts/menu_principal.gd
+++ gods_tabern/scripts/menu_principal.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-
-
+func _ready() -> void:
+	$MainMusic.play()
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/primer_nivel.tscn")

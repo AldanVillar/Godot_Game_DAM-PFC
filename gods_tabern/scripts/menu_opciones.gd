5f589d7b5c6c1b2c8ef841291966127bd25ed31d
Aldan <avillars23@fpcoruna.afundacion.com>
2025-11-29 17:43:44 +0100

    menus principal opciones

---
+++ gods_tabern/scripts/menu_opciones.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),linear_to_db(value))
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
---
+++ gods_tabern/scripts/menu_principal.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/primer_nivel.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu_opciones.tscn")
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
